fa78c12a1e475817947c145475a14b395789dd48
spriest487 <spriest487@googlemail.com>
2017-10-20 20:22:07 +0100

    Class for switching children between parents depending on aspect ratio of container

---
+++ AspectRatioLayoutSwitcher.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace SpaceTrader.Util
+{
+    public class AspectRatioLayoutSwitcher : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform wideRoot;
+
+        [SerializeField]
+        private Transform tallRoot;
+        
+        [ContextMenu("Refresh")]
+        private void OnRectTransformDimensionsChange()
+        {
+            /* find out which layout we should be in */
+            var rect = ((RectTransform)transform).rect;
+            var isWide = rect.width > rect.height;
+
+            /* we have one root for the "tall" layout and one for the "wide"
+             layout - only one of them should ever have children!*/
+            Debug.Assert(wideRoot.childCount == 0 || tallRoot.childCount == 0,
+                $"at least one of the layout roots in {nameof(AspectRatioLayoutSwitcher)} must be empty",
+                this);
+
+            if (wideRoot.childCount == 0 && isWide)
+            {
+                SwitchChildren(tallRoot, wideRoot);
+            }
+            else if (tallRoot.childCount == 0 && !isWide)
+            {
+                SwitchChildren(wideRoot, tallRoot);
+            }
+        }
+
+        private void SwitchChildren(Transform from, Transform to)
+        {
+#if UNITY_EDITOR
+            UnityEditor.Undo.RecordObjects(new[] { from, to }, $"Refresh {nameof(AspectRatioLayoutSwitcher)}");
+#endif
+            for (int child = from.childCount; child > 0; --child)
+            {
+                var childXform = from.GetChild(child - 1);
+                childXform.SetParent(to);
+                childXform.SetAsFirstSibling();
+            }
+        }
+    }
+}
---
+++ Editor/AspectRatioLayoutSwitcherEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SpaceTrader.Util
+{
+    public static class AspectRatioLayoutSwitcherEditor
+    {
+        [MenuItem("SpaceTrader/Refresh All Layout Switchers")]
+        private static void RefreshAllSwitchersInScene()
+        {
+            var switchers = Object.FindObjectsOfType<AspectRatioLayoutSwitcher>();
+            foreach (var switcher in switchers)
+            {
+                switcher.SendMessage("OnRectTransformDimensionsChange");
+
+                Debug.Log($"refreshed layout of {switcher.name}", switcher);
+            }
+        }
+    }
+}
