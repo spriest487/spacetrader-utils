245df8373767b8b4d01f9ff49690b1812f84b585
spriest487 <spriest487@googlemail.com>
2017-10-24 14:46:02 +0100

    Function to save or update asset at a path

---
+++ Editor/AssetUtility.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SpaceTrader.Util
+{
+    public static class AssetUtility
+    {
+        public static void SaveOrUpdate<TAsset>(this TAsset asset, string path)
+            where TAsset : Object
+        {
+            var existing = AssetDatabase.LoadAssetAtPath<TAsset>(path);
+            if (existing)
+            {
+                var srcObj = new SerializedObject(asset);
+                var dstObj = new SerializedObject(existing);
+
+                var propIt = srcObj.GetIterator();
+                if (propIt.Next(true))
+                {
+                    while (propIt.Next(false))
+                    {
+                        dstObj.CopyFromSerializedProperty(propIt);
+                    }
+                }
+                dstObj.ApplyModifiedProperties();
+            }
+            else
+            {
+                AssetDatabase.CreateAsset(asset, path);
+            }
+        }
+    }
+}
