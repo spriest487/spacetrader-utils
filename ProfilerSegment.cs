acd4f929f32e7e8b63560487457801ea1e90b365
spriest487 <spriest487@googlemail.com>
2020-09-14 12:09:52 +0100

    ProfilerSegment

---
+++ ProfilerSegment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine.Profiling;
+using Object = UnityEngine.Object;
+
+namespace SpaceTrader.Util {
+    public readonly struct ProfilerSegment : IDisposable {
+        public ProfilerSegment(string name) {
+            Profiler.BeginSample(name);
+        }
+
+        public ProfilerSegment(string name, Object context) {
+            Profiler.BeginSample(name, context);
+        }
+
+        public void Dispose() {
+            Profiler.EndSample();
+        }
+    }
+}
