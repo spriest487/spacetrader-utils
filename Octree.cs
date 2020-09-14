36cdba321e68d85d38c53abc103121b58da66822
spriest487 <spriest487@googlemail.com>
2020-09-14 12:10:25 +0100

    Octree shrink and search functions

--- Octree.cs
+++ Octree.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace SpaceTrader.Util {
-    public class Octree<T> {
+    public delegate void OctreeNodeVisitorDelegate<TItem, TTime>(
+        Bounds region,
+        IEnumerable<TItem> items,
+        TTime lastModified
+    );
+
+    public class Octree<TItem, TTime> where TTime : IComparable<TTime> {
         private enum Segment {
             WestTopNorth = 0,
             EastTopNorth = 1,
@@ -21,12 +27,18 @@ namespace SpaceTrader.Util {
 
         public Bounds Region => this.root.Region;
 
-        public Octree(Bounds region, float minSize) {
-            this.root = new Node(region, minSize);
+        public Func<TTime> Clock { get; }
+
+        public Octree(Bounds region, float minSize, Func<TTime> clock) {
+            this.Clock = clock;
+
+            this.root = new Node(null, region, minSize, clock());
             this.MinSize = minSize;
         }
 
-        public void Add(Vector3 point, T item) {
+        public void Add(Vector3 point, TItem item) {
+            var timeNow = this.Clock();
+
             if (!this.root.Region.Contains(point)) {
                 var rootRegion = this.root.Region;
 
@@ -39,21 +51,36 @@ namespace SpaceTrader.Util {
                 var parentSize = rootRegion.size * 2;
                 var parentRegion = new Bounds(new Vector3(parentX, parentY, parentZ), parentSize);
                 var oldRoot = this.root;
-                this.root = new Node(parentRegion, this.MinSize);
+                this.root = new Node(null, parentRegion, this.MinSize, timeNow);
 
                 var childSegment = GetChildSegment(parentRegion, rootRegion.center);
                 this.root.children[(int)childSegment] = oldRoot;
+                oldRoot.parent = this.root;
             }
 
-            this.root.Add(point, item, this.MinSize);
+            this.root.Add(point, item, this.MinSize, timeNow);
         }
 
-        public bool Remove(T item) {
-            return this.root.Remove(item);
+        public bool Remove(TItem item) {
+            return this.root.Remove(item, this.Clock());
         }
 
-        public IEnumerable<Bounds> GetAllRegions() {
-            return this.root.GetAllRegions();
+        public void Shrink(TTime cutoff) {
+            this.root.Shrink(cutoff);
+        }
+
+        public void Visit(OctreeNodeVisitorDelegate<TItem, TTime> visitor) {
+            this.root.Visit(visitor);
+        }
+
+        private Node GetNode(Vector3 position) {
+            var node = this.root;
+            while (node.children != null) {
+                var segment = GetChildSegment(node.Region, position);
+                node = node.children[(int)segment];
+            }
+
+            return node;
         }
 
         private static Segment GetChildSegment(Bounds region, Vector3 point) {
@@ -78,14 +105,53 @@ namespace SpaceTrader.Util {
                         : Segment.WestBottomSouth;
         }
 
+        public IEnumerable<TItem> Find(Vector3 origin, float range) {
+            var node = this.GetNode(origin);
+            var rangeSqr = range * range;
+
+            var parent = node.parent;
+            while (parent != null) {
+                var any = false;
+                foreach (var child in parent.children) {
+                    if (child == null || parent == node) {
+                        continue;
+                    }
+
+                    var distSqr = child.Region.SqrDistance(origin);
+                    if (distSqr <= rangeSqr) {
+                        any = true;
+
+                        foreach (var (position, item) in child.GetEntriesDeep()) {
+                            var itemDistSqr = Vector3.SqrMagnitude(position - origin);
+                            if (itemDistSqr <= rangeSqr) {
+                                yield return item;
+                            }
+                        }
+                    }
+                }
+
+                if (any) {
+                    node = parent;
+                    parent = parent.parent;
+                } else {
+                    break;
+                }
+            }
+        }
+
         private class Node {
             internal Bounds Region { get; }
 
+            internal Node parent;
             internal readonly Node[] children;
 
-            private readonly List<KeyValuePair<Vector3, T>> items;
+            private readonly List<KeyValuePair<Vector3, TItem>> items;
 
-            internal Node(Bounds region, float minSize) {
+            private TTime lastModified;
+
+            internal Node(Node parent, Bounds region, float minSize, TTime now) {
+                this.parent = parent;
+                this.lastModified = now;
 
                 var regionSize = region.size;
                 if (regionSize.x < minSize || regionSize.y < minSize || regionSize.z < minSize) {
@@ -104,32 +170,33 @@ namespace SpaceTrader.Util {
                     this.items = null;
                 } else {
                     this.children = null;
-                    this.items = new List<KeyValuePair<Vector3, T>>(0);
+                    this.items = new List<KeyValuePair<Vector3, TItem>>(0);
                 }
             }
 
-            private Node GetOrAddChild(Segment childSegment, float minSize) {
+            private Node GetOrAddChild(Segment childSegment, float minSize, TTime now) {
                 var childIndex = (int)childSegment;
                 if (this.children[childIndex] == null) {
                     var childRegion = this.GetChildRegion(childSegment);
-                    this.children[childIndex] = new Node(childRegion, minSize);
+                    this.children[childIndex] = new Node(this, childRegion, minSize, now);
                 }
 
                 return this.children[childIndex];
             }
 
-            internal void Add(Vector3 position, T item, float minSize) {
+            internal void Add(Vector3 position, TItem item, float minSize, TTime now) {
                 if (this.children != null) {
                     var childIndex = GetChildSegment(this.Region, position);
-                    var child = this.GetOrAddChild(childIndex, minSize);
+                    var child = this.GetOrAddChild(childIndex, minSize, now);
 
-                    child.Add(position, item, minSize);
+                    child.Add(position, item, minSize, now);
                 } else {
-                    this.items.Add(new KeyValuePair<Vector3, T>(position, item));
+                    this.items.Add(new KeyValuePair<Vector3, TItem>(position, item));
+                    this.lastModified = now;
                 }
             }
 
-            public bool Remove(T item) {
+            public bool Remove(TItem item, TTime now) {
                 if (this.items != null) {
                     var itemIndex = this.items.FindIndex(i => i.Value.Equals(item));
                     if (itemIndex == -1) {
@@ -137,11 +204,12 @@ namespace SpaceTrader.Util {
                     }
 
                     this.items.RemoveAt(itemIndex);
+                    this.lastModified = now;
                     return true;
                 }
 
                 foreach (var child in this.children) {
-                    if (child != null && child.Remove(item)) {
+                    if (child != null && child.Remove(item, now)) {
                         return true;
                     }
                 }
@@ -184,17 +252,65 @@ namespace SpaceTrader.Util {
                 }
             }
 
-            internal IEnumerable<Bounds> GetAllRegions() {
-                yield return this.Region;
-
-                if (this.children == null) {
-                    yield break;
+            private bool IsEmpty() {
+                if (this.children != null) {
+                    foreach (var child in this.children) {
+                        if (child != null) {
+                            return false;
+                        }
+                    }
+                    return true;
                 }
 
-                foreach (var child in this.children) {
-                    if (child != null) {
-                        foreach (var childRegion in child.GetAllRegions()) {
-                            yield return childRegion;
+                return this.items.Count == 0;
+            }
+
+            public void Shrink(TTime cutoff) {
+                if (this.children == null) {
+                    return;
+                }
+
+                for (var i = 0; i < this.children.Length; i += 1) {
+                    var child = this.children[i];
+                    if (this.children[i] == null) {
+                        continue;
+                    }
+
+                    this.children[i].Shrink(cutoff);
+
+                    if (child.IsEmpty() && cutoff.CompareTo(child.lastModified) >= 0) {
+                        this.children[i] = null;
+                    }
+                }
+            }
+
+            public void Visit(OctreeNodeVisitorDelegate<TItem, TTime> visitor) {
+                var items = this.items?.Values();
+
+                visitor(this.Region, items, this.lastModified);
+
+                if (this.children != null) {
+                    foreach (var child in this.children) {
+                        if (child != null) {
+                            child.Visit(visitor);
+                        }
+                    }
+                }
+            }
+
+            public IEnumerable<KeyValuePair<Vector3, TItem>> GetEntriesDeep() {
+                if (this.items != null) {
+                    foreach (var entry in this.items) {
+                        yield return entry;
+                    }
+                } else {
+                    foreach (var child in this.children) {
+                        if (child == null) {
+                            continue;
+                        }
+
+                        foreach (var entry in child.GetEntriesDeep()) {
+                            yield return entry;
                         }
                     }
                 }
