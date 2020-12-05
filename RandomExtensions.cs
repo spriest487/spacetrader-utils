aa2aeda4c4c3c50244bfcae10d0fcee421688140
spriest487 <spriest487@googlemail.com>
2020-12-05 06:37:59 +0000

    Random extensions

---
+++ RandomExtensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+namespace SpaceTraderUtils {
+    public static class RandomExtensions {
+        public static float NextFloat(this Random random) {
+            return (float)random.NextDouble();
+        }
+
+        public static float NextFloat(this Random random, float min, float max) {
+            return min + (max - min) * random.NextFloat();
+        }
+
+        public static Vector3 PointOnSphere(this Random random, float radius = 1f) {
+            var x = random.NextFloat();
+            var y = random.NextFloat();
+            var z = random.NextFloat();
+
+            if (x == 0 && y == 0 && z == 0) {
+                return Vector3.forward * radius;
+            }
+
+            var lenInv = 1f / Mathf.Sqrt(x * x + y * y + z * z);
+
+            return new Vector3(x * lenInv, y * lenInv, z * lenInv) * radius;
+        }
+    }
+}
