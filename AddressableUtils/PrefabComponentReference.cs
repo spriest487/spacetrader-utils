72a51f39b5db03bf56b3ab497fd19704e225b611
spriest487 <spriest487@googlemail.com>
2021-01-06 02:32:09 +0000

    Add PrefabComponentReference for addressing prefabs by a component

---
+++ AddressableUtils/PrefabComponentReference.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using Component = System.ComponentModel.Component;
+using Object = UnityEngine.Object;
+
+#if ADDRESSABLES
+
+namespace SpaceTraderUtils.AddressableUtils {
+    [Serializable]
+    public class PrefabComponentReference<TComponent> : AssetReferenceGameObject where TComponent : Component {
+        public PrefabComponentReference(string guid) : base(guid) {
+        }
+
+        public new AsyncOperationHandle<TComponent> LoadAssetAsync() {
+            var loadPrefabOp = base.LoadAssetAsync();
+
+            static AsyncOperationHandle<TComponent> ThenGetComponent(AsyncOperationHandle<GameObject> completedOp) {
+                var component = completedOp.Result.GetComponent<TComponent>();
+                return Addressables.ResourceManager.CreateCompletedOperation(component, null);
+            }
+
+            return Addressables.ResourceManager.CreateChainOperation(loadPrefabOp, ThenGetComponent);
+        }
+
+        public new AsyncOperationHandle<T> LoadAssetAsync<T>() where T : class {
+            var loadAssetOp = this.LoadAssetAsync();
+
+            static AsyncOperationHandle<T> ThenCast(AsyncOperationHandle<TComponent> completedOp) {
+                var result = completedOp.Result as T;
+                return Addressables.ResourceManager.CreateCompletedOperation(result, null);
+            }
+
+            return Addressables.ResourceManager.CreateChainOperation(loadAssetOp, ThenCast);
+        }
+
+        public override bool ValidateAsset(string path) {
+#if UNITY_EDITOR
+            var prefab = AssetDatabase.LoadMainAssetAtPath(path);
+            return this.ValidateAsset(prefab);
+#else
+            return false;
+#endif
+        }
+
+        public override bool ValidateAsset(Object obj) {
+            if (!(obj is GameObject prefab)) {
+                return false;
+            }
+
+            return prefab.TryGetComponent(out TComponent _);
+        }
+    }
+}
+
+#endif
