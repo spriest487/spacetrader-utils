a816bfd0c0702a0efb0765abf27104e545fa27ad
spriest487 <spriest487@googlemail.com>
2021-03-01 01:56:52 +0000

    Added extra methods to prefab asset reference

--- AddressableUtils/PrefabComponentReference.cs
+++ AddressableUtils/PrefabComponentReference.cs
@@ -9,30 +9,89 @@ using Object = UnityEngine.Object;
 
 namespace SpaceTraderUtils.AddressableUtils {
     [Serializable]
-    public class PrefabComponentReference<TComponent> : AssetReferenceGameObject where TComponent : Component {
+    public class PrefabComponentReference<TComponent> : AssetReferenceGameObject
+        where TComponent : Component {
         public PrefabComponentReference(string guid) : base(guid) {
+        }
+
+        private static AsyncOperationHandle<T> ThenGetComponent<T>(AsyncOperationHandle<GameObject> completedOp)
+            where T : class {
+            if (completedOp.Status != AsyncOperationStatus.Succeeded) {
+                var msg = completedOp.OperationException?.Message;
+                return Addressables.ResourceManager.CreateCompletedOperation<T>(null, msg);
+            }
+
+            var component = completedOp.Result.GetComponent<T>();
+            return Addressables.ResourceManager.CreateCompletedOperation(component, null);
         }
 
         public new AsyncOperationHandle<TComponent> LoadAssetAsync() {
             var loadPrefabOp = base.LoadAssetAsync();
-
-            static AsyncOperationHandle<TComponent> ThenGetComponent(AsyncOperationHandle<GameObject> completedOp) {
-                var component = completedOp.Result.GetComponent<TComponent>();
-                return Addressables.ResourceManager.CreateCompletedOperation(component, null);
-            }
-
-            return Addressables.ResourceManager.CreateChainOperation(loadPrefabOp, ThenGetComponent);
+            return Addressables.ResourceManager.CreateChainOperation(loadPrefabOp, ThenGetComponent<TComponent>);
         }
 
-        public new AsyncOperationHandle<T> LoadAssetAsync<T>() where T : class {
-            var loadAssetOp = this.LoadAssetAsync();
+        public new AsyncOperationHandle<TComponent> InstantiateAsync(
+            Transform parent = null,
+            bool instantiateInWorldSpace = false
+        ) {
+            var instantiateOp = base.InstantiateAsync(parent, instantiateInWorldSpace);
+            return Addressables.ResourceManager.CreateChainOperation(instantiateOp, ThenGetComponent<TComponent>);
+        }
 
-            static AsyncOperationHandle<T> ThenCast(AsyncOperationHandle<TComponent> completedOp) {
-                var result = completedOp.Result as T;
-                return Addressables.ResourceManager.CreateCompletedOperation(result, null);
+        public new AsyncOperationHandle<TComponent> InstantiateAsync(
+            Vector3 position,
+            Quaternion rotation,
+            Transform parent = null
+        ) {
+            var instantiateOp = base.InstantiateAsync(position, rotation, parent);
+            return Addressables.ResourceManager.CreateChainOperation(instantiateOp, ThenGetComponent<TComponent>);
+        }
+
+        private static AsyncOperationHandle<T> ThenCast<T>(AsyncOperationHandle<TComponent> completedOp)
+            where T : class {
+            if (completedOp.Status != AsyncOperationStatus.Succeeded) {
+                var msg = completedOp.OperationException?.Message;
+                return Addressables.ResourceManager.CreateCompletedOperation<T>(null, msg);
             }
 
-            return Addressables.ResourceManager.CreateChainOperation(loadAssetOp, ThenCast);
+            var result = completedOp.Result as T;
+            return Addressables.ResourceManager.CreateCompletedOperation(result, null);
+        }
+
+        public new AsyncOperationHandle<T> LoadAssetAsync<T>()
+            where T : class {
+            return Addressables.ResourceManager.CreateChainOperation(this.LoadAssetAsync(), ThenCast<T>);
+        }
+
+        public AsyncOperationHandle<T> InstantiateAsync<T>(
+            Transform parent = null,
+            bool instantiateInWorldSpace = false
+        )
+            where T : class {
+            return Addressables.ResourceManager.CreateChainOperation(
+                this.InstantiateAsync(parent, instantiateInWorldSpace),
+                ThenCast<T>
+            );
+        }
+
+        public AsyncOperationHandle<T> InstantiateAsync<T>(
+            Vector3 position,
+            Quaternion rotation,
+            Transform parent = null
+        )
+            where T : class {
+            return Addressables.ResourceManager.CreateChainOperation(
+                this.InstantiateAsync(position, rotation, parent),
+                ThenCast<T>
+            );
+        }
+
+        public void ReleaseInstance(TComponent instance) {
+            Addressables.ReleaseInstance(instance.gameObject);
+        }
+
+        public void ReleaseInstance(AsyncOperationHandle<TComponent> instance) {
+            Addressables.ReleaseInstance(instance);
         }
 
         public override bool ValidateAsset(string path) {
