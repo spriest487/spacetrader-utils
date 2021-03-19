04b7702a6737235198851accc08e26b9f0f88e7d
spriest487 <spriest487@googlemail.com>
2021-03-19 02:39:16 +0000

    Pool improvements

--- Pool.cs
+++ Pool.cs
@@ -1,57 +1,149 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace SpaceTrader.Util {
-    public class Pool<T> where T : Component {
-        private readonly List<T> active;
-        private readonly Transform parent;
-        private readonly List<T> pooled;
+    public class Pool {
+        public struct PoolCollection : IReadOnlyCollection<Component> {
+            private readonly LinkedList<Component> list;
 
-        private readonly T prefab;
+            public PoolCollection(LinkedList<Component> list) : this() {
+                this.list = list;
+            }
 
-        public Pool(T prefab, Transform parent) {
-            Debug.Assert(!prefab.gameObject.activeSelf, "source prefab for Pool shouldn't be active", prefab);
+            readonly IEnumerator<Component> IEnumerable<Component>.GetEnumerator() {
+                return this.GetEnumerator();
+            }
 
+            readonly IEnumerator IEnumerable.GetEnumerator() {
+                return this.GetEnumerator();
+            }
+
+            public readonly LinkedList<Component>.Enumerator GetEnumerator() {
+                return this.list.GetEnumerator();
+            }
+
+            public readonly int Count => this.list.Count;
+        }
+
+        private readonly LinkedList<Component> active;
+        private readonly LinkedList<Component> pooled;
+
+        private readonly Transform transform;
+
+        private readonly Component prefab;
+
+        public PoolCollection Active => new PoolCollection(this.active);
+        public PoolCollection Pooled => new PoolCollection(this.pooled);
+
+        public Pool(Component prefab, [CanBeNull] Transform parent, string name = "Pool") {
             this.prefab = prefab;
-            this.parent = parent;
 
-            this.pooled = new List<T>();
-            this.active = new List<T>();
+            this.transform = new GameObject(name).transform;
+            this.transform.SetParent(parent, false);
+            this.transform.gameObject.SetActive(false);
+
+            this.pooled = new LinkedList<Component>();
+            this.active = new LinkedList<Component>();
         }
 
         public void Reserve(int minSize) {
             var totalSize = this.pooled.Count + this.active.Count;
             while (totalSize < minSize) {
-                var newItem = Object.Instantiate(this.prefab, this.parent);
+                var newItem = Object.Instantiate(this.prefab, this.transform);
                 Debug.Assert(!newItem.gameObject.activeInHierarchy,
                     "items instantiated during Reserve shouldn't be active", newItem);
-                this.pooled.Add(newItem);
+                this.pooled.AddLast(newItem);
                 ++totalSize;
             }
         }
 
-        public void Return(T item) {
+        public void Return(Component item) {
             Debug.Assert(!this.pooled.Contains(item));
             Debug.Assert(this.active.Contains(item));
 
-            item.gameObject.SetActive(false);
-            this.pooled.Add(item);
+            item.transform.SetParent(this.transform);
+
+            this.pooled.AddLast(item);
             this.active.Remove(item);
         }
 
-        public T Get() {
-            T item;
-            if (this.pooled.Count > 0) {
-                item = this.pooled[0];
-                this.pooled.RemoveAt(0);
-            } else {
-                item = Object.Instantiate(this.prefab, this.parent);
+        public Component Get(Transform parent) {
+            if (this.pooled.Count == 0) {
+                this.Reserve(Mathf.Max(1, this.active.Count * 2));
             }
 
-            this.active.Add(item);
-            item.gameObject.SetActive(true);
+            var item = this.pooled.First.Value;
+            this.pooled.RemoveFirst();
+            this.active.AddLast(item);
+
+            item.transform.SetParent(parent);
 
             return item;
+        }
+
+        public void ReturnAll() {
+            foreach (var item in this.active) {
+                this.Return(item);
+            }
+        }
+    }
+
+    public class Pool<T> : Pool where T : Component {
+        public new struct PoolCollection : IReadOnlyCollection<T> {
+            public struct Enumerator : IEnumerator<T> {
+                private LinkedList<Component>.Enumerator listEnumerator;
+
+                public Enumerator(LinkedList<Component>.Enumerator listEnumerator) {
+                    this.listEnumerator = listEnumerator;
+                }
+
+                public bool MoveNext() {
+                    return this.listEnumerator.MoveNext();
+                }
+
+                void IEnumerator.Reset() {
+                }
+
+                public T Current => (T)this.listEnumerator.Current;
+
+                object IEnumerator.Current => this.Current;
+
+                public void Dispose() {
+                    this.listEnumerator.Dispose();
+                }
+            }
+
+            private readonly Pool.PoolCollection collection;
+
+            public PoolCollection(Pool.PoolCollection collection) : this() {
+                this.collection = collection;
+            }
+
+            IEnumerator<T> IEnumerable<T>.GetEnumerator() {
+                return this.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() {
+                return this.GetEnumerator();
+            }
+
+            public readonly Enumerator GetEnumerator() {
+                return new Enumerator(this.collection.GetEnumerator());
+            }
+
+            public readonly int Count => this.collection.Count;
+        }
+
+        public new PoolCollection Active => new PoolCollection(base.Active);
+        public new PoolCollection Pooled => new PoolCollection(base.Pooled);
+
+        public Pool(T prefab, [CanBeNull] Transform parent, string name = "Pool") : base(prefab, parent, name) {
+        }
+
+        public new T Get(Transform parent) {
+            return (T)base.Get(parent);
         }
     }
 }
