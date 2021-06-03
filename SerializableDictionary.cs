9c79a5ce70fa3fc068fee61a109968ac9328ec00
spriest487 <spriest487@googlemail.com>
2021-06-03 19:38:35 +0100

    SerializableDictionary

---
+++ SerializableDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+namespace SpaceTrader.Util {
+    [Serializable]
+    internal class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver {
+        [SerializeField]
+        private List<TKey> keys;
+
+        [SerializeField]
+        private List<TValue> values;
+
+        public SerializableDictionary() {
+            this.keys = new List<TKey>();
+            this.values = new List<TValue>();
+        }
+
+        public SerializableDictionary(IDictionary<TKey, TValue> input) : base(input) {
+            this.keys = new List<TKey>();
+            this.values = new List<TValue>();
+        }
+
+        public void OnBeforeSerialize() {
+            this.keys.Clear();
+            this.values.Clear();
+
+            foreach (var (key, value) in this) {
+                this.keys.Add(key);
+                this.values.Add(value);
+            }
+        }
+
+        public void OnAfterDeserialize() {
+            this.Clear();
+
+            if (this.keys.Count != this.values.Count) {
+                throw new SerializationException(
+                    $"there are {this.keys.Count} keys and {this.values.Count} "
+                    + "values after deserialization. Make sure that both key and value types are serializable."
+                );
+            }
+
+            for (var i = 0; i < this.keys.Count; i++) {
+                this.Add(this.keys[i], this.values[i]);
+            }
+        }
+    }
+}
