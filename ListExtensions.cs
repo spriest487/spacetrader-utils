c3e3c47e8febe2b9f2a6bc5f6e1f9eb1e5b32a42
spriest487 <spriest487@googlemail.com>
2021-06-27 02:47:27 +0100

    Add Fisher-Yates shuffle function for lists

--- ListExtensions.cs
+++ ListExtensions.cs
@@ -57,5 +57,16 @@ namespace SpaceTrader.Util {
 
             return default; // unreachable
         }
+
+        public static void Shuffle<T>(this IList<T> list) {
+            var n = list.Count;
+            while (n > 1) {
+                n--;
+                var k = UnityRandom.Range(0, n + 1);
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
     }
 }
