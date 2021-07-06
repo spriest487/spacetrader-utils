00e703b8ac24c848969aabe5c22a8b1053909698
spriest487 <spriest487@googlemail.com>
2021-07-06 13:51:53 +0100

    SubmeshTextureMap: add Clear function to reuse instances

--- SubmeshTextureMap.cs
+++ SubmeshTextureMap.cs
@@ -3,31 +3,38 @@ using UnityEngine;
 
 namespace SpaceTrader.Util {
     public class SubmeshTextureMap {
-        private readonly Dictionary<Texture2D, int> textureSubmeshes;
+        private readonly Dictionary<Texture2D, int> submeshByTexture;
 
-        private List<Texture2D> textures;
+        private readonly List<Texture2D> textures;
         public IReadOnlyList<Texture2D> Textures => this.textures;
 
         private int nextIndex;
 
         public SubmeshTextureMap() {
-            this.textureSubmeshes = new Dictionary<Texture2D, int>();
+            this.submeshByTexture = new Dictionary<Texture2D, int>();
             this.textures = new List<Texture2D>();
 
             this.nextIndex = 0;
         }
         
         public int GetSubmeshIndex(Texture2D texture) {
-            if (this.textureSubmeshes.TryGetValue(texture, out var index)) {
+            if (this.submeshByTexture.TryGetValue(texture, out var index)) {
                 return index;
             }
 
             index = this.nextIndex;
-            this.textureSubmeshes.Add(texture, index);
+            this.submeshByTexture.Add(texture, index);
             this.textures.Add(texture);
 
             this.nextIndex += 1;
             return index;
+        }
+
+        public void Clear() {
+            this.textures.Clear();
+            this.submeshByTexture.Clear();
+
+            this.nextIndex = 0;
         }
     }
 }
