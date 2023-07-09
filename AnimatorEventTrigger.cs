fe10fc806bd32717403dbd1b022705d69e5a19f1
spriest487 <spriest487@googlemail.com>
2023-07-09 20:36:59 +0100

    Generic event trigger for animators

---
+++ AnimatorEventTrigger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SpaceTrader.Util {
+    [RequireComponent(typeof(Animator))]
+    public class AnimatorEventTrigger : MonoBehaviour {
+        [Serializable]
+        private class Trigger {
+            public string Key;
+            public UnityEvent<AnimatorEventTrigger> Action;
+        }
+
+        [SerializeField]
+        private List<Trigger> triggers;
+
+        private void OnDestroy() {
+            foreach (var trigger in this.triggers) {
+                trigger.Action.RemoveAllListeners();
+            }
+        }
+
+        public void AddListener(string key, UnityAction<AnimatorEventTrigger> action) {
+            if (!this.GetTrigger(key, out var trigger)) {
+                trigger = new Trigger {
+                    Key = key,
+                    Action = new UnityEvent<AnimatorEventTrigger>(),
+                };
+
+                this.triggers.Add(trigger);
+            }
+            
+            trigger.Action.AddListener(action);
+        }
+
+        public void RemoveListener(string key, UnityAction<AnimatorEventTrigger> action) {
+            if (this.GetTrigger(key, out var trigger)) {
+                trigger.Action.RemoveListener(action);
+            }
+        }
+
+        private bool GetTrigger(string key, [MaybeNullWhen(false)] out Trigger keyTrigger) {
+            foreach (var trigger in this.triggers) {
+                if (string.Equals(key, trigger.Key, StringComparison.Ordinal)) {
+                    keyTrigger = trigger;
+                    return true;
+                }
+            }
+
+            keyTrigger = default;
+            return false;
+        }
+
+        [UsedImplicitly]
+        private void OnAnimationEvent(string key) {
+            if (this.GetTrigger(key, out var trigger)) {
+                trigger.Action.Invoke(this);
+            }
+        }
+    }
+}
