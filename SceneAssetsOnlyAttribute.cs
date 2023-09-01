ad043901c684958c1b8c5c26709f38c16d52ab4b
spriest487 <spriest487@googlemail.com>
2023-09-01 01:47:31 +0100

    Attribute for showing only scene assets

---
+++ SceneAssetsOnlyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SpaceTrader.Util {
+    public class SceneAssetsOnlyAttribute : AssetReferenceUIRestriction {
+        public override bool ValidateAsset(Object obj) {
+#if UNITY_EDITOR
+            return this.ValidateAsset(AssetDatabase.GetAssetPath(obj));
+#else
+            return base.ValidateAsset(obj);
+#endif
+        }
+
+        public override bool ValidateAsset(string path) {
+#if UNITY_EDITOR
+            var assetType = AssetDatabase.GetMainAssetTypeAtPath(path);
+            return assetType == typeof(SceneAsset);
+#else
+            return base.ValidateAsset(path);
+#endif
+        }
+    }
+}
