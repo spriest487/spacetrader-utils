7433e35859e70226da1a78e0e1c7b39a764aafd4
spriest487 <spriest487@googlemail.com>
2024-05-03 16:58:42 +0100

    Add EditorPrefsAssetReference

---
+++ Editor/EditorPrefsAssetReference.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SpaceTrader.Util.EditorUtil {
+    public class EditorPrefsAssetReference<T> where T : Object {
+        public string Name { get; }
+
+        private readonly string pathKey;
+        private T instance;
+
+        public string Path {
+            get => EditorPrefs.GetString(this.pathKey, "");
+            set {
+                EditorPrefs.SetString(this.pathKey, value);
+                this.instance = null;
+            }
+        }
+
+        public T Instance {
+            get {
+                if (!this.instance && !string.IsNullOrWhiteSpace(this.Path)) {
+                    this.instance = AssetDatabase.LoadAssetAtPath<T>(this.Path);
+                    if (!this.instance) {
+                        this.Path = "";
+                    }
+                }
+
+                return this.instance;
+            }
+        }
+
+        public EditorPrefsAssetReference(string name) {
+            this.Name = name;
+            this.pathKey = $"EditorPrefsAssetReference.{name}";
+        }
+    }
+}
